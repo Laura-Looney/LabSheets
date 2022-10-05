ce505f49274eb2e85cba68415bf747c53e39fc56
STUDENT Laura Looney <Laura.Looney@students.ittralee.ie>
2022-10-05 14:50:14 +0100

    Initial commit

---
+++ src/Books/Book.java
@@ -0,0 +1,19 @@
+package Books;
+
+public class Book
+    {
+        private String Title;
+        private String ISBN;
+        private double Price;
+        private int Pages;
+
+        public Book() //no argument constructor. The default for no values.
+            {
+                Title = "Not Available";
+                ISBN = "Not Available";
+                Price = 00.00;
+                Pages = 0;
+            }
+
+
+    }
---
+++ src/Books/TestBook.java
@@ -0,0 +1,13 @@
+package Books;
+
+import javax.swing.*;
+
+public class TestBook
+    {
+        public static void main(String[] args)
+            {
+                String output="";
+
+                //output+=
+            }
+    }
